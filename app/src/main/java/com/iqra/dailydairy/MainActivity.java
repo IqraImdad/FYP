3bcacf7625ca340321f9efb58fb6ca13c3a8475c
IqraImdad <bila5224552@gmail.com>
2019-12-18 20:36:22 +0500

    second-commit

--- app/src/main/java/com/iqra/dailydairy/MainActivity.java
+++ app/src/main/java/com/iqra/dailydairy/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    private void sum(int a , int b)
+    {
+        
+    }
 }
