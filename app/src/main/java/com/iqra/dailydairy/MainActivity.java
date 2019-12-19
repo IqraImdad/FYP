b1d3c0a171e4278fb248a7d4d0012b27bc755ede
IqraImdad <bila5224552@gmail.com>
2019-12-19 19:59:53 +0500

    curent_date_next_and_pre

--- app/src/main/java/com/iqra/dailydairy/MainActivity.java
+++ app/src/main/java/com/iqra/dailydairy/MainActivity.java
@@ -3,13 +3,68 @@ package com.iqra.dailydairy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.iqra.dailydairy.utils.MonthProvider;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
-    @Override
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton btnNextMonth, btnPreviousMonth;
+    TextView tvSelectedMonth;
+    ArrayList<String> months;
+    Calendar calendar;
+    Date currentDate;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
     }
-    
+
+    private void initComponents() {
+        btnNextMonth = findViewById(R.id.btnNextMonth);
+        btnPreviousMonth = findViewById(R.id.btnLastMonth);
+        btnNextMonth.setOnClickListener(this);
+        btnPreviousMonth.setOnClickListener(this);
+        tvSelectedMonth = findViewById(R.id.tvSelectedMonth);
+
+        calendar = Calendar.getInstance();
+        currentDate = calendar.getTime();
+        tvSelectedMonth.setText(formatDate(currentDate));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnNextMonth) {
+            nextMonth();
+        }
+        if (v == btnPreviousMonth) {
+            previousMonth();
+        }
+    }
+
+    private void nextMonth() {
+        calendar.add(Calendar.MONTH, 1);
+        currentDate = calendar.getTime();
+        tvSelectedMonth.setText(formatDate(currentDate));
+    }
+
+    private void previousMonth() {
+        calendar.add(Calendar.MONTH, -1);
+        currentDate = calendar.getTime();
+        tvSelectedMonth.setText(formatDate(currentDate));
+    }
+
+    private String formatDate(Date date) {
+        SimpleDateFormat df = new SimpleDateFormat("MMM-yyyy");
+        return df.format(date);
+    }
 }
--- app/src/main/java/com/iqra/dailydairy/utils/MonthProvider.java
+++ app/src/main/java/com/iqra/dailydairy/utils/MonthProvider.java
@@ -1,7 +1,13 @@
 package com.iqra.dailydairy.utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MonthProvider {
+    private static ArrayList<String> months = new ArrayList<String>(Collections.singletonList("January,February,March,April,May,June,July,August,September,October,November,December"));
 
+    public static ArrayList<String> getMonths() {
+        return months;
+    }
 }
