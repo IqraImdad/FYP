8d0a1bdd8ad4e7fc44fd3b4336a5a140b2329f3c
junaid ijaz <junaidijaz88@gmail.com>
2020-02-03 01:13:21 +0500

    blunder-for-chain-activity-resolved

--- app/src/main/java/com/iqra/dailydairy/ChainsEventActivity.java
+++ app/src/main/java/com/iqra/dailydairy/ChainsEventActivity.java
@@ -5,10 +5,7 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -35,8 +32,6 @@ public class ChainsEventActivity extends AppCompatActivity {
 
     RecyclerView rvChainsEvent;
     String selectedYear, selectedDay, selectedMonth;
-    String fromSelectedYear, fromSelectedDay, fromSelectedMonth;
-    String toSelectedYear, toSelectedDay, toSelectedMonth;
     ChainsEventsAdapter adapter;
     ArrayList<Event> events = new ArrayList<>();
     Chain chain = new Chain();
@@ -44,9 +39,7 @@ public class ChainsEventActivity extends AppCompatActivity {
     String id = "";
     ChainDao chainDao;
     EventDao eventDao;
-    RadioGroup rgEvents;
-    LinearLayout llFT;
-    TextView tvFrom, tvNDI, tvNoEvents;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,127 +71,20 @@ public class ChainsEventActivity extends AppCompatActivity {
     }
 
     private void buildRecyclerView() {
-        if (events.size() < 1) {
-            tvNoEvents.setVisibility(View.VISIBLE);
-            rvChainsEvent.setAdapter(null);
-        } else {
-            tvNoEvents.setVisibility(View.GONE);
-            rvChainsEvent.setLayoutManager(new LinearLayoutManager(this));
-            adapter = new ChainsEventsAdapter(events);
-            rvChainsEvent.setAdapter(adapter);
-            adapter.setOnClickListener(this::showDatePicker);
-        }
-
+        rvChainsEvent.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ChainsEventsAdapter(events);
+        rvChainsEvent.setAdapter(adapter);
+        adapter.setOnClickListener(this::showDatePicker);
 
     }
 
 
     private void initComponents() {
         rvChainsEvent = findViewById(R.id.rvChainsEvents);
-        rgEvents = findViewById(R.id.rgEvents);
-        llFT = findViewById(R.id.llFT);
-        tvFrom = findViewById(R.id.tvFrom);
-        tvNDI = findViewById(R.id.tvNoDI);
-        tvNoEvents = findViewById(R.id.tvNoEvents);
-
-        llFT.setOnClickListener(view -> showDatePickerForFromDate());
-
-        rgEvents.setOnCheckedChangeListener((radioGroup, i) -> {
-            if (i == R.id.rbAllEvents) {
-                RadioButton btn = radioGroup.findViewById(R.id.rbAllEvents);
-                if (btn.isChecked()) {
-                    llFT.setVisibility(View.GONE);
-                    getAllEvents();
-                    sortEvents();
-                    buildRecyclerView();
-                    tvNDI.setVisibility(View.GONE);
-                }
-            } else {
-                RadioButton btn = radioGroup.findViewById(R.id.rbRange);
-                if (btn.isChecked()) {
-                    showNoIntervalMsg();
-                    showDatePickerForFromDate();
-                    llFT.setVisibility(View.VISIBLE);
-
-                }
-            }
-        });
-
     }
 
-    private void showDatePickerForFromDate() {
-
-        DatePickerDialog mPicker;
-        Calendar calendar = Calendar.getInstance();
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        int month = calendar.get(Calendar.MONTH);
-        int year = calendar.get(Calendar.YEAR);
-
-        Toast.makeText(this, "Select Event Starting Date", Toast.LENGTH_SHORT).show();
-        // date picker dialog
-        mPicker = new DatePickerDialog(this,
-                (view, year1, monthOfYear, dayOfMonth) -> {
-
-                    fromSelectedDay = String.valueOf(dayOfMonth);
-                    fromSelectedMonth = String.valueOf(monthOfYear + 1);
-                    fromSelectedYear = String.valueOf(year1);
-
-                    showDatePickerForToDate();
-
-                }, year, month, day);
-
-        mPicker.setOnCancelListener(dialogInterface -> showNoIntervalMsg());
-
-        mPicker.setTitle("From");
-        mPicker.setCancelable(false);
-        mPicker.show();
-
-    }
-
-    private void showNoIntervalMsg() {
-        rvChainsEvent.setAdapter(null);
-        tvNDI.setVisibility(View.VISIBLE);
-
-    }
-
-    private void showDatePickerForToDate() {
-        Toast.makeText(this, "Select Event Ending Date", Toast.LENGTH_SHORT).show();
-        DatePickerDialog mPicker;
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Integer.valueOf(fromSelectedYear), Integer.valueOf(fromSelectedMonth) - 1, Integer.valueOf(fromSelectedDay));
-        // date picker dialog
-        calendar.setFirstDayOfWeek(Integer.valueOf(fromSelectedDay));
-        calendar.setFirstDayOfWeek(Integer.valueOf(fromSelectedDay));
-        calendar.setFirstDayOfWeek(Integer.valueOf(fromSelectedDay));
-
-        mPicker = new DatePickerDialog(this,
-                (view, year1, monthOfYear, dayOfMonth) -> {
-
-                    toSelectedDay = String.valueOf(dayOfMonth);
-                    toSelectedMonth = String.valueOf(monthOfYear + 1);
-                    toSelectedYear = String.valueOf(year1);
-
-                    String from = fromSelectedDay + "-" + fromSelectedMonth + "-" + fromSelectedYear;
-                    String to = toSelectedDay + "-" + toSelectedMonth + "-" + toSelectedYear;
-                    tvFrom.setText(from + " to " + to);
-
-                    ArrayList<Event> e = getNewList((ArrayList<Event>) eventDao.getAllEvents(), from, to);
-                    events.clear();
-                    events.addAll(e);
-
-                    buildRecyclerView();
 
 
-                }, Integer.valueOf(fromSelectedYear), Integer.valueOf(fromSelectedMonth) - 1, Integer.valueOf(fromSelectedDay));
-
-        mPicker.setOnCancelListener(dialogInterface -> showNoIntervalMsg());
-
-        mPicker.setTitle("To");
-        mPicker.getDatePicker().setMinDate(calendar.getTimeInMillis());
-        mPicker.setCancelable(false);
-        mPicker.show();
-
-    }
 
 
     private void showDatePicker(int pos) {
@@ -257,29 +143,7 @@ public class ChainsEventActivity extends AppCompatActivity {
 
     }
 
-    private ArrayList<Event> getNewList(ArrayList<Event> oldList, String from, String to) {
-        ArrayList<Event> newList = new ArrayList<Event>();
-        Date d1 = null, d2 = null, d3 = null, d4 = null;
-        SimpleDateFormat f = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
 
-        try {
-            d3 = f.parse(from);
-            d4 = f.parse(to);
-            for (int i = 0; i < oldList.size(); i++) {
-
-                String b = oldList.get(i).getlastaction();
-
-                d2 = f.parse(b);
-
-                if (d2.compareTo(d3) >= 0 && d2.compareTo(d4) <= 0) {
-                    newList.add(oldList.get(i));
-                }
-            }
-        } catch (Exception ex) {
-
-        }
-        return newList;
-    }
 
     private void sortEvents() {
         if (events.size() > 1) {
--- app/src/main/java/com/iqra/dailydairy/CreateNewChainActivity.java
+++ app/src/main/java/com/iqra/dailydairy/CreateNewChainActivity.java
@@ -1,9 +1,13 @@
 package com.iqra.dailydairy;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,18 +20,29 @@ import com.iqra.dailydairy.room.ChainDao;
 import com.iqra.dailydairy.room.EventDao;
 import com.iqra.dailydairy.room.MyRoomDatabase;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNewChainActivity extends AppCompatActivity implements OnCheckBoxClicked, View.OnClickListener {
 
     RecyclerView rvEvents;
     Button btnSaveChain;
     EditText etChainName;
-    TextView tvNd;
     EventDao eventDao;
     ChainDao chainDao;
     ArrayList<Event> selectedEvents;
     ArrayList<Event> events;
+    RadioGroup rgEvents;
+    LinearLayout llFT;
+    String fromSelectedYear, fromSelectedDay, fromSelectedMonth;
+    String toSelectedYear, toSelectedDay, toSelectedMonth;
+    TextView tvFrom, tvNoEvents;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,7 @@ public class CreateNewChainActivity extends AppCompatActivity implements OnCheck
         chainDao = MyRoomDatabase.getDatabase(this).chainDao();
         selectedEvents = new ArrayList<>();
         initComponents();
+        events = (ArrayList<Event>) eventDao.getAllEvents();
         buildRecyclerView();
 
     }
@@ -47,18 +63,94 @@ public class CreateNewChainActivity extends AppCompatActivity implements OnCheck
         rvEvents = findViewById(R.id.rvEvents);
         btnSaveChain = findViewById(R.id.btnSaveChain);
         etChainName = findViewById(R.id.etChainName);
-        tvNd = findViewById(R.id.tvNd);
+        tvNoEvents = findViewById(R.id.tvNd);
         btnSaveChain.setOnClickListener(this);
+
+        rgEvents = findViewById(R.id.rgEvents);
+        llFT = findViewById(R.id.llFT);
+        tvFrom = findViewById(R.id.tvFrom);
+
+        llFT.setOnClickListener(view -> showDatePickerForFromDate());
+
+        rgEvents.setOnCheckedChangeListener((radioGroup, i) -> {
+            if (i == R.id.rbAllEvents) {
+                RadioButton btn = radioGroup.findViewById(R.id.rbAllEvents);
+                if (btn.isChecked()) {
+                    llFT.setVisibility(View.GONE);
+                    getAllEvents();
+                    sortEvents();
+                    buildRecyclerView();
+                }
+            } else {
+                selectedEvents.clear();
+                RadioButton btn = radioGroup.findViewById(R.id.rbRange);
+                if (btn.isChecked()) {
+                    showNoIntervalMsg();
+                    showDatePickerForFromDate();
+                    llFT.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+    }
+
+    private void sortEvents() {
+        if (events.size() > 1) {
+            Collections.sort(events, new Comparator<Event>() {
+                public int compare(Event o1, Event o2) {
+                    if (o1.getDate() == null || o2.getDate() == null)
+                        return 0;
+                    return o1.getDate().compareTo(o2.getDate());
+                }
+            });
+        }
+    }
+
+    private void getAllEvents() {
+        events.clear();
+        events.addAll(eventDao.getAllEvents());
+    }
+
+    private void showDatePickerForFromDate() {
+
+        DatePickerDialog mPicker;
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+
+        Toast.makeText(this, "Select Event Starting Date", Toast.LENGTH_SHORT).show();
+        // date picker dialog
+        mPicker = new DatePickerDialog(this,
+                (view, year1, monthOfYear, dayOfMonth) -> {
+
+                    fromSelectedDay = String.valueOf(dayOfMonth);
+                    fromSelectedMonth = String.valueOf(monthOfYear + 1);
+                    fromSelectedYear = String.valueOf(year1);
+
+                    showDatePickerForToDate();
+
+                }, year, month, day);
+
+        mPicker.setOnCancelListener(dialogInterface -> showNoIntervalMsg());
+
+        mPicker.setTitle("From");
+        mPicker.setCancelable(false);
+        mPicker.show();
+
+    }
+
+    private void showNoIntervalMsg() {
+        rvEvents.setAdapter(null);
     }
 
     private void buildRecyclerView() {
-        events = (ArrayList<Event>) eventDao.getEvents();
 
         if (events.size() < 1) {
-            tvNd.setVisibility(View.VISIBLE);
+            tvNoEvents.setVisibility(View.VISIBLE);
             return;
         } else {
-            tvNd.setVisibility(View.GONE);
+            tvNoEvents.setVisibility(View.GONE);
         }
 
         rvEvents.setLayoutManager(new LinearLayoutManager(this));
@@ -67,6 +159,69 @@ public class CreateNewChainActivity extends AppCompatActivity implements OnCheck
         adapter.setCheckBoxClickListener(this);
         rvEvents.setAdapter(adapter);
 
+    }
+
+
+    private void showDatePickerForToDate() {
+        Toast.makeText(this, "Select Event Ending Date", Toast.LENGTH_SHORT).show();
+        DatePickerDialog mPicker;
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Integer.valueOf(fromSelectedYear), Integer.valueOf(fromSelectedMonth) - 1, Integer.valueOf(fromSelectedDay));
+        // date picker dialog
+        calendar.setFirstDayOfWeek(Integer.valueOf(fromSelectedDay));
+        calendar.setFirstDayOfWeek(Integer.valueOf(fromSelectedDay));
+        calendar.setFirstDayOfWeek(Integer.valueOf(fromSelectedDay));
+
+        mPicker = new DatePickerDialog(this,
+                (view, year1, monthOfYear, dayOfMonth) -> {
+
+                    toSelectedDay = String.valueOf(dayOfMonth);
+                    toSelectedMonth = String.valueOf(monthOfYear + 1);
+                    toSelectedYear = String.valueOf(year1);
+
+                    String from = fromSelectedDay + "-" + fromSelectedMonth + "-" + fromSelectedYear;
+                    String to = toSelectedDay + "-" + toSelectedMonth + "-" + toSelectedYear;
+                    tvFrom.setText(from + " to " + to);
+
+                    ArrayList<Event> e = getNewList((ArrayList<Event>) eventDao.getAllEvents(), from, to);
+                    events.clear();
+                    events.addAll(e);
+                    buildRecyclerView();
+
+
+                }, Integer.valueOf(fromSelectedYear), Integer.valueOf(fromSelectedMonth) - 1, Integer.valueOf(fromSelectedDay));
+
+        mPicker.setOnCancelListener(dialogInterface -> showNoIntervalMsg());
+
+        mPicker.setTitle("To");
+        mPicker.getDatePicker().setMinDate(calendar.getTimeInMillis());
+        mPicker.setCancelable(false);
+        mPicker.show();
+
+    }
+
+    private ArrayList<Event> getNewList(ArrayList<Event> oldList, String from, String to) {
+        ArrayList<Event> newList = new ArrayList<Event>();
+        Date d1 = null, d2 = null, d3 = null, d4 = null;
+        SimpleDateFormat f = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+
+        try {
+            d3 = f.parse(from);
+            d4 = f.parse(to);
+            for (int i = 0; i < oldList.size(); i++) {
+
+                String b = oldList.get(i).getlastaction();
+
+                d2 = f.parse(b);
+
+                if (d2.compareTo(d3) >= 0 && d2.compareTo(d4) <= 0) {
+                    newList.add(oldList.get(i));
+                }
+            }
+        } catch (Exception ex) {
+
+        }
+        return newList;
     }
 
     @Override
